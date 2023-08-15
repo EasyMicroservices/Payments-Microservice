a5bdd7a67eab8a1f67ab53493613ad312c02b7b1
Elyas Amini <elyas.amini.fs@gmail.com>
2023-08-15 13:13:57 +0330

    Init project

---
+++ src/CSharp/EasyMicroservices.PaymentsMicroservice.Domain/Contracts/Requests/ProudctCreateRequestContract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMicroservices.PaymentsMicroservice.Contracts.Requests
+{
+    public class ProudctCreateRequestContract
+    {
+        public long InvoiceId { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public string UniqueIdentity { get; set; }
+    }
+}
---
+++ src/CSharp/EasyMicroservices.PaymentsMicroservice.Domain/Contracts/Requests/ProudctUpdateRequestContract.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMicroservices.PaymentsMicroservice.Contracts.Requests
+{
+    public class ProudctUpdateRequestContract
+    {
+        public long Id { get; set; }
+        public long InvoiceId { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public string UniqueIdentity { get; set; }
+    }
+}
